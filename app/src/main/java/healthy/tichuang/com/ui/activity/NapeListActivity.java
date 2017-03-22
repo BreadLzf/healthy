7cbba4f538308d64bbefecbfb59f80aa1a2bb008
Punisher <punisher@PunisherdeMacBook-Pro.local>
2017-03-22 20:15:05 +0800

    颈部详情布局初步完成

---
+++ app/src/main/java/healthy/tichuang/com/ui/activity/NapeListActivity.java
@@ -0,0 +1,18 @@
+package healthy.tichuang.com.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import healthy.tichuang.com.ui.base.BaseActivity;
+
+/**
+ * Created by punisher on 2017/3/22.
+ */
+
+public class NapeListActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
